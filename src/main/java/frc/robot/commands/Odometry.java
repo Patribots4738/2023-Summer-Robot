ce4b774ab396e2f5f285891dd6027d4304d2ff0f
Jacob Hotz <lordgaming1010@gmail.com>
2023-07-07 10:58:14 -0700

    remove unused imports

--- src/main/java/frc/robot/commands/Odometry.java
+++ src/main/java/frc/robot/commands/Odometry.java
@@ -1,14 +1,11 @@
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
 import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Drivetrain;
 
 public class Odometry extends CommandBase {
