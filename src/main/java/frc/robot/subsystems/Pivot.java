4dd9ee7f5030d8307b3e99f35e73148a3bc2102d
Jacob1010-h <lordgaming1010@gmail.com>
2023-07-14 17:19:51 -0700

    classify params for the claw and pivot motors

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -1,46 +1,74 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.PivotConstants;
 
 public class Pivot extends SubsystemBase {
   private double rotation;
 
-  // TODO: Declare Motors and Encoders
-  // HINT: Copy the formatting of the drivetrain
   CANSparkMax pivotLead;
   CANSparkMax pivotFollower;
 
+  SparkMaxPIDController pivotPIDController;
+
+  private final AbsoluteEncoder pivotEncoder;
 
   public Pivot() {
-    // TODO: make this the value of the motor at start
-    this.rotation = 0.0; 
-    
-    // TODO: Initialize Motors and Encoders
-    pivotLead = new CANSparkMax(0, MotorType.kBrushless);
-    pivotFollower = new CANSparkMax(1, MotorType.kBrushless);
+    this.rotation = 0.0;
+
+    // Initialize the motors
+    pivotLead = new CANSparkMax(PivotConstants.PIVOT_LEAD_CAN_ID, MotorType.kBrushless);
+    pivotFollower = new CANSparkMax(PivotConstants.PIVOT_FOLLOWER_CAN_ID, MotorType.kBrushless);
+
+    // Restore the motors to factory defaults
+    pivotLead.restoreFactoryDefaults();
+    pivotFollower.restoreFactoryDefaults();
 
     pivotFollower.follow(pivotLead);
+
+    pivotPIDController = pivotLead.getPIDController();
+
+    pivotEncoder = pivotLead.getAbsoluteEncoder(Type.kDutyCycle);
+
+    // Convert the encoder position from rotations to degrees
+    pivotEncoder.setPositionConversionFactor(PivotConstants.PIVOT_POSITION_ENCODER_FACTOR);
+    
+    pivotLead.setSmartCurrentLimit(PivotConstants.PIVOT_SMART_CURRENT_LIMIT);
+
   }
 
+  // TODO: add this to the periodic method of the robot container
+  public void periodic() {
+    pivotPIDController.setReference(this.rotation, ControlType.kPosition);
+  }
 
-  
   /**
    * Get the current rotation of the pivot
+   * 
    * @return the current rotation of the pivot
    */
   public double getRotationDegrees() {
-    return this.rotation;
+    return rotation;
+  }
+
+  public double getEncoderPosition() {
+    return pivotEncoder.getPosition();
   }
 
   /**
    * Set the rotation of the pivot, in degrees
+   * 
    * @param placementPositionDegrees the rotation of the pivot
    */
   public void setRotation(double placementPositionDegrees) {
-    this.rotation = placementPositionDegrees;
+    rotation = placementPositionDegrees;
   }
-  
+
 }
