ca246e13c8e5b69e9de2147f488f01c46f4b6e9c
ChadPad <patribotsprogramming4738@gmail.com>
2023-07-19 14:48:56 -0700

    add slew rate limiting and invert lead motor

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -2,44 +2,58 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ClawConstants;
 
 public class Claw extends SubsystemBase {
+
   private double speed;
 
   private final CANSparkMax leadMotor;
   private final CANSparkMax followerMotor;
 
+  private final SlewRateLimiter filter;
+
 
   public Claw() {
     speed = 0.0;
+
+    filter = new SlewRateLimiter(ClawConstants.SLEW_POSITIVE_LIMIT, ClawConstants.SLEW_NEGATIVE_LIMIT, 0);
 
     leadMotor = new CANSparkMax(ClawConstants.CLAW_LEAD_CAN_ID, CANSparkMax.MotorType.kBrushless);
     Constants.SPARK_LIST.add(leadMotor);
     followerMotor = new CANSparkMax(ClawConstants.CLAW_FOLLOWER_CAN_ID, CANSparkMax.MotorType.kBrushless);
     Constants.SPARK_LIST.add(followerMotor);
 
+    
     leadMotor.restoreFactoryDefaults();
     followerMotor.restoreFactoryDefaults();
-
+    
+    leadMotor.setInverted(true);
     leadMotor.setSmartCurrentLimit(ClawConstants.CLAW_STALL_CURRENT_LIMIT, ClawConstants.CLAW_FREE_CURRENT_LIMIT);
     
     followerMotor.follow(leadMotor, true);
 
     // Flash is burnt in robotContainer... incinerateMotors()
+
+    // registers the periodic method each program loop
+    super.register();
+
   }
 
   // Set the speed of the motors, positive is out, negative is in
   // range: -1, 1
   public void setSpeed(double speed) {
+    leadMotor.setSmartCurrentLimit(
+        speed < 0 ? ClawConstants.CLAW_OUTTAKE_CURRENT_LIMIT : ClawConstants.CLAW_STALL_CURRENT_LIMIT, 
+        ClawConstants.CLAW_FREE_CURRENT_LIMIT);
     this.speed = speed;
-    periodic();
   }
 
   // TODO: add this to the periodic method of the robot container
   public void periodic() {
-    leadMotor.set(speed);
+    leadMotor.set(filter.calculate(speed));
   }
 }
