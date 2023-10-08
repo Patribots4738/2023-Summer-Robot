585907949e6afb779b9e2224616ddafa6af44248
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2023-10-08 13:55:56 -0700

    add sim capabilities

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -30,7 +31,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
-    Logger.configureLoggingAndConfig(robotContainer, false);
+    if(RobotBase.isReal()){
+      Logger.configureLoggingAndConfig(robotContainer, false);
+    }
     
   }
 
@@ -48,7 +51,9 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
-    Logger.updateEntries();
+    if(RobotBase.isReal()){
+      Logger.updateEntries();
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
