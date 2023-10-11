2847a12c2e3b5a9b5b3cd73890eb1a14b436229b
Jacob Hotz <lordgaming1010@gmail.com>
2023-10-11 15:18:19 -0700

    add autolevel to autoprograms, and complete P1_M_C
    Co-authored-by: @YoungChadawan

--- src/main/java/frc/robot/auto/AutoPrograms.java
+++ src/main/java/frc/robot/auto/AutoPrograms.java
@@ -5,13 +5,11 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.commands.AutoLevel;
 import frc.robot.commands.PlaceHighBackThenTraj;
-import frc.robot.commands.StopDrive;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.limelight.LimelightBase;
 import java.util.HashMap;
 import java.util.Set;
@@ -48,7 +46,13 @@ public class AutoPrograms {
         // TODO: add more auto programs here
         constraints = new PathConstraints(2, DrivetrainConstants.MAX_DRIVE_ACCELERATION);
         PlaceHighBackThenTraj placeHighBack = new PlaceHighBackThenTraj("Charge", constraints);
-        auto.put("DEFAULT", placeHighBack);
+        AutoLevel autoLevel = new AutoLevel();
+        SequentialCommandGroup P1_Mobility_Charge = new SequentialCommandGroup(
+            placeHighBack,
+            autoLevel
+        );
+
+        auto.put("DEFAULT", P1_Mobility_Charge);
 
         Set<String> keySet = auto.keySet();
         String[] keySetCopy = new String[keySet.size()];
--- src/main/java/frc/robot/auto/LoadPath.java
+++ src/main/java/frc/robot/auto/LoadPath.java
@@ -5,17 +5,11 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPRamseteCommand;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.ControllerConstants;
 import frc.robot.Constants.DrivetrainConstants;
-import frc.robot.Constants.PlacementConstants;
 import frc.robot.commands.ResetPose;
 import frc.robot.commands.StopDrive;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Pivot;
 
 public class LoadPath {
     
