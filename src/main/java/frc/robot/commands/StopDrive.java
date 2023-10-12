5a6eb905248a85657779a828dad76defe9abe5ea
Jacob Hotz <lordgaming1010@gmail.com>
2023-10-11 17:22:46 -0700

    make stop drive instant command

--- src/main/java/frc/robot/commands/StopDrive.java
+++ src/main/java/frc/robot/commands/StopDrive.java
@@ -1,24 +1,17 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Drivetrain;
 
-public class StopDrive extends CommandBase {
-    private static Command command = new InstantCommand(() -> Drivetrain.getInstance().drive(0, 0));
+public class StopDrive extends InstantCommand {
+    private static Runnable command = () -> Drivetrain.getInstance().drive(0, 0);
     
     public StopDrive(){
-        addRequirements(Drivetrain.getInstance());
-    }
-    
-    @Override
-    public void initialize() {
-        command.execute();
+        super(command);
     }
 
-    public static Command getCommand(){
-      return command;
+    public InstantCommand getCommand(){
+      return this;
     }
     
 }
